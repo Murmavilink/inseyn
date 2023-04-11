bfa7549ba3c72e7bd22e3a194c5bf7b60f39ed0d
Mur <murmavilink@gmail.com>
2023-04-11 15:39:22 +0600

    refactoring menu

--- src/modules/navHeader.js
+++ src/modules/navHeader.js
@@ -19,7 +19,7 @@ export const menu = () => {
     const menuBtn = document.querySelector('.menu__icon');
     const popupMenu = document.querySelector('.popup-menu');
     const popupContentMenu = document.querySelector('.popup-dialog-menu');
-    const closeBtn = popupMenu.querySelector('.close-menu');
+    const popupContentTypes = document.querySelector('.popup-repair-types');
 
 
     menuBtn.addEventListener('click', () => {
@@ -29,5 +29,35 @@ export const menu = () => {
     });
 
 
-    closeBtn.addEventListener('click', () => popupContentMenu.style = '');
+    popupMenu.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if(e.target.classList.contains('menu-link')) {
+            const id = e.target.getAttribute('href').substring(1);
+            const section = document.getElementById(id);
+    
+    
+            if(section) {
+                section.scrollIntoView({
+                    block: "center", 
+                    behavior: "smooth"
+                });
+            }
+            
+            popupContentMenu.style = '';
+        }
+
+        if(e.target.classList.contains('close-menu')) popupContentMenu.style = '';
+
+        if(e.target.classList.contains('no-overflow')) {
+            popupContentTypes.style.visibility = 'visible';
+            popupContentMenu.style = '';
+        }
+    });
+
+
+    popupContentTypes.addEventListener('click', (e) => {
+        if(!e.target.closest('.popup-dialog-repair-types')) popupContentTypes.style.visibility = 'hidden';
+    });
+  
 };
